21a1a7644ff11e5070e65b440bf9888cfa9ad596
cohensam <cohensam@seas.upenn.edu>
2014-09-05 04:19:57 -0400

    Adding files

---
+++ Frogger/Assets/MoveFrogScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFrogScript : MonoBehaviour {
+
+	public float move;
+	public int stepsForward;
+	public int stepsSideways;
+
+	// Use this for initialization
+	void Start () {
+		move = .5f;
+		stepsForward = 0;
+		stepsSideways = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow) && stepsForward < 100) {
+			gameObject.transform.Translate (0, 0, move);
+			stepsForward++;
+		} else if (Input.GetKey (KeyCode.DownArrow) && stepsForward > 0) {
+			gameObject.transform.Translate (0, 0, -move);
+			stepsForward--;
+		} else if (Input.GetKey (KeyCode.LeftArrow) && stepsSideways > -70) {
+			gameObject.transform.Translate (-move, 0, 0);
+			stepsSideways--;
+		} else if (Input.GetKey (KeyCode.RightArrow) && stepsSideways < 70) {
+			gameObject.transform.Translate (move, 0, 0);
+			stepsSideways++;
+		}
+	}
+ }
---
+++ Frogger/Assets/MoveLeftScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftScript : MonoBehaviour {
+
+	public float move;
+
+	// Use this for initialization
+	void Start () {
+		move = -.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (gameObject.hits screen end collider, then put at x position -39.38337
+		gameObject.transform.Translate (0, 0, move);
+	}
+}
